26f06f8a652786d5f159a4f7b37a712fcc9061cf
Henry <ngthhuy98@gmail.com>
2026-05-29 17:48:06 +0700

    Revised the xpath exercise

--- ThanhHuy/BTBuoi03/xpath.py
+++ ThanhHuy/BTBuoi03/xpath.py
@@ -1,7 +1,7 @@
 '''
 - Menu Magage Clients
-    1.//span[normalize-space()='Manage Clients']
-    2.(//a[@class='pc-link '])[8]/span[@class='pc-mtext']
+    1.//a[@href='https://hrm.anhtester.com/erp/clients-list']
+    2.//span[normalize-space()='Manage Clients']/parent::a
 
 - Button [+ Add New]
     1.//a[normalize-space()='Add New']
@@ -10,31 +10,41 @@
 - Textbox: First Name
     1.//input[@name='first_name']
     2.//label[@for='company_name']/following-sibling::div/input
+
 - Textbox: Last Name
     1.//input[@name='last_name']
     2.//label[@class='control-label']/following-sibling::div//input
+
 - Textbox: Password
     1.//input[@name='password']
     2.(//div[@class='input-group-prepend'])[3]/following-sibling::input
+
 - Textbox: Contact Number
     1.//input[@placeholder='Contact Number']
     2.//label[@for='contact_number']/following-sibling::input
+
 - Textbox: Email
     1. //input[@placeholder='Email']
     2.//label[contains(text(),'Email')]/following-sibling::div/input
+
 - Textbox: Username
     1.//input[@placeholder='Username']
     2.//label[contains(text(),'Username')]/following-sibling::div/input
+    
+
 - Dropdown list: Gender 
-    1.//span[@id='select2-gender-5z-container']
-    2.//span[@class='selection']//span[@role='textbox']
+    1.//span[@role="combobox"]//span[@role="textbox"]
+    2.//span[@class='select2-selection__arrow']/preceding-sibling::span
+
 - Button: Reset
     1.//button[normalize-space()='Reset']
-    2.//div[@class='card-footer text-right']//button[@type='reset']
+    2.//button[@type='submit']/preceding-sibling::button[normalize-space()='Reset']
+
 - Button: Save
-    1.//span[normalize-space()='Save']
+    1.//button[@type='reset']/following-sibling::button
     2.//div[@class='card-footer text-right']//button[@type='submit']
+
 - Input
     1.//input[@name='file']
-    2.//div[@class='custom-file']//input
+    2.//label[@class='custom-file-label']/preceding-sibling::input
 '''
--- ThanhHuy/BTBuoi04/tests/test_BTBuoi4.py
+++ ThanhHuy/BTBuoi04/tests/test_BTBuoi4.py
@@ -1,20 +1,22 @@
+from xmlrpc import client
+
 from playwright.sync_api import Page
 import time
 
 def test_actions_BTBuoi4(page:Page):
-    print("Start open URL")
+    
+    #Start open URL
     page.goto("https://hrm.anhtester.com/erp/login")
     page.get_by_role('textbox', name='Your Username').fill("admin_example")
     page.get_by_role('textbox', name='Enter Password').fill("123456")
     page.locator('//button[contains(@class,"btn-primary")]').click()
-    time.sleep(5)
 
-    print("Click menu ManageClients")
+    #Click menu ManageClients
     page.locator('//span[normalize-space()="Manage Clients"]').click()
     page.locator('//a[normalize-space()="Add New"]').click()
     time.sleep(5)
 
-    print("Fill information")
+    #Fill information
     page.get_by_role('textbox', name='First Name').fill("ThanhHuy")
     page.get_by_role('textbox', name='Last Name').fill("Nguyen")
     page.get_by_role('textbox', name='Password').fill("123456")
@@ -24,4 +26,4 @@ def test_actions_BTBuoi4(page:Page):
     page.locator('//select[@name="gender"]').select_option("Male")
     page.locator('//input[@name="file"]').set_input_files("ThanhHuy/BTBuoi04/data/attached_file.png")
     page.locator('//span[normalize-space()="Save"]').click()
-    time.sleep(5)
+    
