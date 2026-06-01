253ffade8638a3a3b1b53d18bebe601c510aad31
“liuduyquang” <liuduyquang.97@gmail.com>
2026-06-01 17:04:17 +0700

    Chỉnh sửa bài tập buổi 4 và 5

--- DuyQuang/BT_Buoi4/tests/test_bt_action.py
+++ DuyQuang/BT_Buoi4/tests/test_bt_action.py
@@ -3,26 +3,50 @@ from playwright.sync_api import Playwright, Page, sync_playwright, expect
 import time
 
 def test_add_user(page : Page):
+    # Go to login page
     page.goto("https://hrm.anhtester.com/")
-    page.get_by_role("textbox", name="Your Username").fill("admin_example")
-    page.get_by_role("textbox", name="Enter Password").fill("123456")
-    page.locator("//i[@class='fas fa-user-lock d-blockd']").click()
-    # time.sleep(5)
 
+    # Input username
+    page.get_by_role("textbox", name="Your Username").fill("admin_example")
+
+    # Input password
+    page.get_by_role("textbox", name="Enter Password").fill("123456")
+
+    # Click Login button    
+    page.locator("//i[@class='fas fa-user-lock d-blockd']").click()
+
+    # Click Manage Clients menu
     page.get_by_role("link", name="Manage Clients").click()
+
+    # Click Add new
     page.get_by_role("link", name="Add New").click()
+
+    # Input First name
     page.get_by_role("textbox", name="First Name").fill("Liu")
+
+    # Input Last name
     page.get_by_role("textbox", name="Last Name").fill("Quang")
+
+    # Input password
     page.get_by_role("textbox", name="Password").fill("123456")
+
+    # Input Contact number
     page.locator("//input[@name='contact_number']").fill("0359848555")
+
+    # Select Gender
     page.locator("//select[@class='form-control select2-hidden-accessible']").select_option("Female")
+
+    # Input Email
     page.get_by_role("textbox", name="Email").fill("liuduyquang@gmail.com")
+
+    # Input Username
     page.get_by_role("textbox", name="Username").fill("LiuQuang")
-    time.sleep(5)
-    page.locator("//input[@name='file' and contains(@class, 'custom-file-input')]").set_input_files("tests/uploadimg.png")
-    time.sleep(5)
+
+    # Attach file
+    page.locator("//input[@name='file' and contains(@class, 'custom-file-input')]").set_input_files("DuyQuang/BT_Buoi4/tests/uploadimg.png")
+
+    # Click save button
     page.get_by_role("button", name="Save").click()
-    time.sleep(5)
 
 
 
--- DuyQuang/BT_Buoi5/tests/test_assertions.py
+++ DuyQuang/BT_Buoi5/tests/test_assertions.py
@@ -3,25 +3,25 @@ from playwright.sync_api import Playwright, Page, sync_playwright, expect
 import time
 
 def test_add_user(page : Page):
+    # Go to login page
     page.goto("https://hrm.anhtester.com/")
-    page.get_by_role("textbox", name="Your Username").fill("admin_example")
-    page.get_by_role("textbox", name="Enter Password").fill("123456")
-    page.locator("//i[@class='fas fa-user-lock d-blockd']").click()
-    # time.sleep(5)
 
+    # Input username
+    page.get_by_role("textbox", name="Your Username").fill("admin_example")
+
+    # Input password
+    page.get_by_role("textbox", name="Enter Password").fill("123456")
+
+    # Click Login button    
+    page.locator("//i[@class='fas fa-user-lock d-blockd']").click()
+
+    # Click Manage Clients menu
     page.get_by_role("link", name="Manage Clients").click()
-    # page.get_by_role("link", name="Add New").click()
-    # page.get_by_role("textbox", name="First Name").fill("Liu")
-    # page.get_by_role("textbox", name="Last Name").fill("Quang")
-    # page.get_by_role("textbox", name="Password").fill("123456")
-    # page.locator("//input[@name='contact_number']").fill("0359848555")
-    # page.locator("//select[@class='form-control select2-hidden-accessible']").select_option("Female")
-    # page.get_by_role("textbox", name="Email").fill("liuduyquang@gmail.com")
-    # page.get_by_role("textbox", name="Username").fill("LiuQuang")
-    # page.locator("//input[@name='file' and contains(@class, 'custom-file-input')]").set_input_files("tests/uploadimg.png")
-    # page.get_by_role("button", name="Save").click()
+
+    # Search for user by email
     page.get_by_role("searchbox", name="Search").fill("liuduyquang@gmail.com")
-    time.sleep(5)
+
+    # Verify search results
     expect(page.locator("//h6[@class='m-b-0']")).to_contain_text("Liu Quang")
     expect(page.locator("//p[@class='m-b-0']")).to_contain_text("liuduyquang@gmail.com")
     expect(page.locator("//div[@class='d-inline-block align-middle']/following::td[1]")).to_contain_text("LiuQuang")
