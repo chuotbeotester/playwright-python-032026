5af7d61eaf25faa38e8dc5a483bf65ef22c03f79
panda15-06 <trangtuyen1524@gmail.com>
2026-06-13 22:43:53 +0700

    Trang update BT5

--- Huyen_Trang/BTBuoi5/tests/test_action_b5.py
+++ Huyen_Trang/BTBuoi5/tests/test_action_b5.py
@@ -1,6 +1,14 @@
 from playwright.sync_api import Page, expect 
 import time 
 
+first_name = "Trang4"
+last_name = "Nguyen"
+pass_word = "123456@"
+email = "trang6@gmail.com"
+user_name = "Huyentrang6"
+contact_number = "0666666666"
+gender = "Male"
+
 def test_action(page: Page):
     page.goto("https://hrm.anhtester.com/")
 
@@ -16,28 +24,28 @@ def test_action(page: Page):
     page.get_by_role("link", name="Add New").click()
 
     #Fill fields and save 
-    page.get_by_role("textbox", name="First Name").fill("Trang4")
-    page.get_by_role("textbox", name="Last Name").fill("Nguyen")
-    page.get_by_role("textbox", name="Password").fill("123456@")
-    page.get_by_role("textbox", name="Email").fill("trang4@gmail.com")
-    page.get_by_role("textbox", name="Username").fill("Huyentrang4")
-    page.locator('input[name="contact_number"]').fill("0666666666")
-    page.locator('//label[normalize-space()="Gender"]/following-sibling::select').select_option(label="Female")
-    page.get_by_role("button", name="Choose File").set_input_files("tests/image.jpg")
+    page.get_by_role("textbox", name="First Name").fill(first_name)
+    page.get_by_role("textbox", name="Last Name").fill(last_name)
+    page.get_by_role("textbox", name="Password").fill(pass_word)
+    page.get_by_role("textbox", name="Email").fill(email)
+    page.get_by_role("textbox", name="Username").fill(user_name)
+    page.locator('input[name="contact_number"]').fill(contact_number)
+    page.locator('//label[normalize-space()="Gender"]/following-sibling::select').select_option(label=gender)
+    page.locator("//input[@type='file']").set_input_files("Huyen_Trang/BTBuoi5/tests/image.jpg")
     
     page.get_by_role("button", name="Save").click()
 
     #Search email
-    page.get_by_role("searchbox", name="Search").fill("trang4@gmail.com")
+    page.get_by_role("searchbox", name="Search").fill(email)
     time.sleep(3)
 
     #Check info in table 
-    expect(page.locator('//h6[@class="m-b-0"]')).to_have_text("Trang4 Nguyen")
-    expect(page.locator('//p[@class="m-b-0"]')).to_have_text("trang4@gmail.com")
-    expect(page.get_by_role("gridcell", name="Huyentrang4")).to_have_text("Huyentrang4")
-    expect(page.get_by_role("gridcell", name="0666666666")).to_have_text("0666666666")
-    expect(page.get_by_role("gridcell", name="Female")).to_have_text("Female")
-    expect(page.get_by_role("gridcell", name="Active")).to_have_text("Active")
+    expect(page.locator('//h6[@class="m-b-0"]')).to_have_text(first_name+" "+last_name)
+    expect(page.locator('//p[@class="m-b-0"]')).to_have_text(email)
+    expect(page.locator("//tbody/tr[1]/td[2]")).to_have_text(user_name)
+    expect(page.locator("//tbody/tr[1]/td[3]")).to_have_text(contact_number)
+    expect(page.locator("//tbody/tr[1]/td[4]")).to_have_text(gender)
+    expect(page.locator("//tbody/tr[1]/td[6]")).to_have_text("Active")
 
     time.sleep(3)
 
