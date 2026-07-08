8e6142f25cfcb8b495c374c5ae0e4c908313b838
phuonghoanguyen97 <phuonghoanguyen97@gmail.com>
2026-07-08 21:41:25 +0700

    feat:hoan thanh bai tap buoi 04

---
+++ PhuongHoa/BTBuoi04/test_actions_and_wait.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import Page
+
+def test_actions(page: Page):
+    print("Test actions for HRM web")
+
+    #Login vao trang HRM
+    page.goto('https://hrm.anhtester.com/')
+    page.get_by_role('textbox', name='Your Username').fill('admin_example')
+    page.get_by_role('textbox', name='Enter Password').fill('123456')
+    page.locator('//button[@type="submit"]').click()
+
+    #Vao trang Manage Clients
+    page.get_by_role('link', name="Manage Clients").click()
+    page.locator('//a[normalize-space()="Add New"]').click()
+    page.get_by_role('textbox', name='First Name').fill('Hoa')
+    page.get_by_role('textbox', name='Last Name').fill('Nguyen')
+    page.get_by_role('textbox', name='Password').fill('123456')
+    page.locator('//input[@name="contact_number"]').fill('0966123456')
+    page.locator('//select[@name="gender"]').select_option(index=1)
+    page.get_by_role('textbox', name='Email').fill('hoanp@gmail.com')
+    page.get_by_role('textbox', name='Username').fill('HoaNP')
+    page.locator('//input[@type="file"]').set_input_files('PhuongHoa/BTBuoi04/image.jpg')
+    page.locator('//button[normalize-space() = "Save"]').click()
+  
+
+    print("Create an user successfully")
